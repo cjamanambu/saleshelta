f4b646f2b601ca099a255d6fd7c99f8c67725ee1
CJ Amanambu <oamanambu@yahoo.com>
2022-01-09 23:26:33 +0100

    optimized header slider images

--- src/layouts/app-default-layout.vue
+++ src/layouts/app-default-layout.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <Header />
-    <transition name="fade" mode="out-in">
-      <slot />
-    </transition>
-    <Footer />
+    <PageLoader v-if="false" />
+    <div v-else>
+      <Header />
+      <transition name="fade" mode="out-in">
+        <slot />
+      </transition>
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/header";
 import Footer from "./components/footer";
+import PageLoader from "./components/page-loader";
 
 export default {
   components: {
     Header,
     Footer,
+    PageLoader,
   },
 };
 </script>
